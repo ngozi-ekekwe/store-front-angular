470fcccdbedeb307d768322a189dbfb8412a157a
Ngozi Ekekwe <ngozi-ekekwe>
2021-08-29 20:56:26 +0200

    make sure routing works

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./modules/home/home.component"
+import {PrdouctDetailComponent} from './modules/prdouct-detail/prdouct-detail.component'
+import {SuccessPageComponent} from "./modules/success-page/success-page.component"
+import {CartComponent} from "./modules/cart/cart.component"
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'product-detail/:id',
+    component: PrdouctDetailComponent
+  },
+  {
+    path: 'success',
+    component: SuccessPageComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
+import { AppComponent } from './app.component';
 import { PrdouctDetailComponent } from './modules/prdouct-detail/prdouct-detail.component';
 import { CartComponent } from './modules/cart/cart.component';
 import { SuccessPageComponent } from './modules/success-page/success-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { SuccessPageComponent } from './modules/success-page/success-page.compon
     SuccessPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
